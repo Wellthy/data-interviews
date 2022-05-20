6634138c462d85485cd00528dddeb048d5d5c320
Brian Toole <brian@wellthy.com>
2022-05-20 09:29:51 -0700

    Logic simplification

--- Engineer/prep/intermediate/full_project_history.sql
+++ Engineer/prep/intermediate/full_project_history.sql
@@ -7,14 +7,14 @@ SELECT history.project_id
      , history.created_date
      , history.first_active_date
      , history.modified_date
-     , MIN(CASE WHEN status.description = 'Paused'
+     , MIN(CASE WHEN history.project_status = 'Paused'
                 THEN history.modified_date
                 ELSE NULL
             END) OVER (PARTITION BY history.project_id) AS first_paused_date
      , RANK() OVER (PARTITION BY history.project_id
                         ORDER BY history.project_history_id DESC) AS history_sequence
-	 , max(case when status.description = 'Complete' 
-	 			then coalesce(history.modified_date, projects.modified_date) --modified_date
+	 , max(case when history.project_status = 'Complete' 
+	 			then history.modified_date
 	 			else null
 	 		end) over (partition by history.project_id) as last_completed_date
      , projects.project_deleted_date

fba18bb288fc9ce32f550f8a01d4f9e50e3d3e85
Brian Toole <brian@wellthy.com>
2022-05-26 10:34:44 -0700

    Updates to models and yml

--- Engineer/prod/current_task_summary.sql
+++ Engineer/prod/current_task_summary.sql
@@ -6,7 +6,6 @@ select distinct
     full_task_history.project_id,
     full_task_history.task_status,
     full_task_history.first_in_progress_date,
-    full_task_history.first_assigned_to_adviser_date,
     full_task_history.last_completed_date
 from {{ ref('full_task_history') }} full_task_history
 where full_task_history.history_sequence = 1
